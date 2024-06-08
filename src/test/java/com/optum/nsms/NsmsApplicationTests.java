124ab2b79d00297b9981ab257a135a7cc41eb487
nareshboodida3 <140226972+nareshboodida3@users.noreply.github.com>
2024-06-08 22:23:35 +0530

    test end point added

--- src/test/java/com/optum/nsms/NsmsApplicationTests.java
+++ src/test/java/com/optum/nsms/NsmsApplicationTests.java
@@ -3,11 +3,15 @@ package com.optum.nsms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
 class NsmsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		 
 	}
 
 }
