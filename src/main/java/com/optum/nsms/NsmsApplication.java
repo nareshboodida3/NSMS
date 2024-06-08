2922b43578a52821c6c84b50aec99e8629f94fce
nareshboodida3 <140226972+nareshboodida3@users.noreply.github.com>
2024-06-08 21:37:23 +0530

    initial commit

---
+++ src/main/java/com/optum/nsms/NsmsApplication.java
@@ -0,0 +1,13 @@
+package com.optum.nsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsmsApplication.class, args);
+	}
+
+}
